ca8f0a76a594f70339d1e88cbd48ce6a92e356ab
xyhxx <603331487@qq.com>
2024-08-27 22:46:09 +0800

    fix: 修改引入方式

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
-import * as css from './index.module.css';
+import css from './index.module.css';
 
 function App() {
   const [count, setCount] = useState(0);
